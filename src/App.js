8059630c82a8a6e6bea420b95ea0b55e91ea4448
Damian Cannon <Damian Cannon>
2016-11-06 11:26:37 +0000

    Get list of messages and extract post links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import xhr from 'xhr';
 
 class App extends Component {
   state = {
@@ -9,6 +10,31 @@ class App extends Component {
   fetchData = (evt) => {
     evt.preventDefault();
 	
+	xhr({
+	  // valuemargin boards:
+      url: 'http://boards.fool.co.uk/high-yield-hyp-practical-51676.aspx?mid=11105157&sort=username'
+      //url: 'http://boards.fool.co.uk/high-yield-share-strategies-51166.aspx?mid=10237021&sort=username'
+    }, function (err, data) {
+      /* …save the data here */
+	  //console.log([...data.body.querySelectorAll('a')].filter(x => x.href.includes('boards.fool.co.uk/Messages.asp')));
+	  
+	  // Messages in this table: tblMessagesAsp
+	  var element = document.createElement('div');
+	  element.insertAdjacentHTML('beforeend', data.body);
+	  var tableOfPosts = element.querySelector('#tblMessagesAsp');
+	  var nextLink = element.querySelector('.nextLink');
+	  
+	  // Show table of posts
+	  var thing = document.querySelector('#contents');
+	  thing.insertAdjacentElement('beforeend', tableOfPosts);
+	  thing.insertAdjacentElement('beforeend', nextLink);
+	  
+	  // Get list of post links
+	  var postLinks = [...document.querySelectorAll('a')].filter(x => x.href.includes('Message.aspx')).map(h => h.href);
+	  console.log(postLinks);
+	  
+    });
+	
     this.setState({
       boards: ['http://boards.fool.co.uk/a-fool-and-his-money-51365.aspx?mid=6808140&sort=username']
     });	
@@ -17,10 +43,19 @@ class App extends Component {
   render() {
     return (
       <div>
+		<div>
 		<h1>Motley Fool Downloader</h1>
-        <form onSubmit={this.fetchData}>
-			<button>Get boards for user</button>
-        </form>
+			<form onSubmit={this.fetchData}>
+			  <label>I want to get the posts from this board
+				<input placeholder={"link to board"} type="text" />
+			  </label>
+			  <label>for this user
+				<input placeholder={"user name"} type="text" />
+			  </label>
+			  <button>Get data</button>
+			</form>		
+		</div>
+		<div id="contents"></div>
       </div>
     );
   }
